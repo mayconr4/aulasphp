ee3dfda54c1846cf6a8f1c00e6696f4036ad12ae
mayconr4 <mayconhs2007@gmail.com>
2025-02-04 11:31:35 -0300

    adiciona array em objeto

--- 04-objetos-genericos.php
+++ 04-objetos-genericos.php
@@ -36,13 +36,24 @@ echo "<p><mark>$usuario->nome</mark></p>"; //interploração
     <p>celular: <?=$usuario->telefones[1] ?></p>
 
     <hr>
-    
+
     <h2>convertendo  (fazendo um casting) um objeto em um array associativo</h2>
 
 <?php $arrayUsuario = (array) $usuario; ?>
 
     <h3>Analisando o array gerado a partir de um objeto</h3>
-    <pre><?=var_dump($arrayUsuario)?></pre>
+    <pre><?=var_dump($arrayUsuario)?></pre> 
+
+    <h2>Convertendo (fazendo um casting) array associativo em objeto</h2>
+
+<?php
+// array associativo com duas chaves (nome e sobrenome)
+$aluno = ["nome" => "Fulano", "sobrenome" => "dos Santos"];
+
+$objAluno = (object) $aluno;
+?>
+    <h3>Analisando o objeto gerado a partir de um array</h3>
+    <pre><?=var_dump($objAluno)?></pre>
 
     
 </body>
