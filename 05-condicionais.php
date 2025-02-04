d4b01c4af3ce44c72caea4bff631429cbcca1827
mayconr4 <mayconhs2007@gmail.com>
2025-02-04 11:42:13 -0300

    adiciona exemplo de omitir chaves de if

---
+++ 05-condicionais.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php - condicionais</title>
+</head>
+<body>
+    <h1>Estruturas condicionais</h1>
+    <hr>
+
+    <h2>Simples usando <code>if</code></h2>
+   
+<?php
+$numero = 5;
+
+
+if($numero > 1){
+    echo "<p>$numero é maior que 1</p>";
+} 
+
+// sintaxe omitindo as chaves
+if($numero > 1) echo "<p>$numero é maior que 1</p>";
+?>
+
+    <h2>Composta</h2>
+
+    <h2>Encadeada</h2>
+
+</body>
+</html>
