b455c23bc7f29c880b1328699bef9c397475f2e3
mayconr4 <mayconhs2007@gmail.com>
2025-02-04 11:16:07 -0300

    adiciona exemplos de declaração a objeto

--- 04-objetos-genericos.php
+++ 04-objetos-genericos.php
@@ -11,11 +11,32 @@
 
 <?php
 // Criando uma instancia de uma classe genérica chamada stdClass (Stander Class)
-$usuario = new stdClass(); //instancia ou objeto
+$usuario = new stdClass(); //instancia ou objeto 
+
+// adicionando propriedades/atributos ao objeto
+$usuario ->nome = "Chapolin colorado";
+$usuario ->idade = 25;
+$usuario ->email = "chapolin@gmail.com";
+$usuario->telefones = ["11 97504-0149", "11 90000-9999"]; 
+
+echo $usuario->nome;
+
+// pode ser usadp {} ao redor do objeto/propriedade na interploração
+echo "<p><mark>$usuario->nome</mark></p>"; //interploração
 ?>
 
 
     <h2>Analisando a estrtura do objeto</h2>
     <pre><?=var_dump($usuario)?>></pre>    
+    <hr>
+
+    <h2>saida de dados</h2>
+    <p>Nome: <?=$usuario->nome?></p>
+    <p>idade: <?=$usuario->idade?> anos</p>
+    <p>celular: <?=$usuario->telefones[1] ?></p>
+
+    <hr>
+    
+    
 </body>
 </html>
