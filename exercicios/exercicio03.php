2acf8ff6f72503a0a8a060f9798127cb1b162b59
mayconr4 <mayconhs2007@gmail.com>
2025-02-05 11:47:31 -0300

    soluciona desafio de formatação

---
+++ exercicios/exercicio03.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exrcicio-03</title>
+</head>
+<body>
+    <h1>Exrcicio-03</h1> 
+
+
+<?php
+$valorDaCompra = 8000;  
+
+if ($valorDaCompra > 5000) {
+   $desconto = 0.15 ;
+} elseif ($valorDaCompra > 3000 ) {
+  $desconto = 0.10;
+} elseif ($valorDaCompra > 1000) {
+    $desconto = 0.5; 
+} else  {
+    $desconto = 0;
+}
+
+ $valorFinal = $valorDaCompra - $valorDaCompra * $desconto; 
+ //$valorFinal = $valorDaCompra - $desconto; 
+
+ $valorFinal_formatado = number_format($valorFinal, 2, ',','.');
+
+?> 
+
+<p>O valor final da compra é R$<?=$valorFinal_formatado?></p>
+    
+</body>
+</html>
