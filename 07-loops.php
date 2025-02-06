866c90d8930f8903d6ce30aa28203da44c747478
mayconr4 <mayconhs2007@gmail.com>
2025-02-06 09:53:09 -0300

    adiciona exemplo do loop for

--- 07-loops.php
+++ 07-loops.php
@@ -36,9 +36,18 @@ do {
 <?php 
     $j++;
 } while($j <= 5);
-?>
+?> 
 
+    <hr>
     <h2>FOR (PARA)</h2>
-    <p>Executa ações por uma <b>quantidade determinada</b> de vezes.</p>    
+    <p>Executa ações por uma <b>quantidade determinada</b> de vezes.</p>
+    
+<?php
+for( $i = 1; $i <= 10; $i++ ){
+?>
+    <p><code>i</code> vale: <b> <?=$i?> </b></p>
+<?php
+}
+?>
 </body>
 </html>
