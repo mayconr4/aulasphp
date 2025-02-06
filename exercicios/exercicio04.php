c7c43629939a0161a884234a29130c32cc82f295
mayconr4 <mayconhs2007@gmail.com>
2025-02-06 11:42:25 -0300

    exercicio-04

---
+++ exercicios/exercicio04.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exrcicio - 04</title>
+</head>
+<body>
+    <h1>EXERCICIO - 04 </h1>
+<?php
+$linguagems = new stdClass();  
+$linguagems->$html = "Estruturação";
+$linguagems->$css = "Estilos";
+$linguagems->$js = "Comportamentos";
+$linguagems->$php = "Back-end";
+$linguagems->$sql = "Manipulação de dados";
+$linguagems->$java = "Softwares";
+?> 
+
+<thead></thead>
+    
+</body>
+</html>
