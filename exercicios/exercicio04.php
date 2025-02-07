649927492a207946407a0b455cb23ae710eb52b4
mayconr4 <mayconhs2007@gmail.com>
2025-02-07 09:27:38 -0300

    adiciona outro array

--- exercicios/exercicio04.php
+++ exercicios/exercicio04.php
@@ -7,19 +7,18 @@
      
     <style> 
 
-  table {
-            width: 50%;
-            border-collapse: collapse;
-        }
-        table, td { border: 1px solid black;}
-           
-        
-        th, td {
-            padding: 5px;
-            text-align: left;
-        }        
+    table { width: 50%; border-collapse:collapse; }        
+
+    table, td  { border: 1px solid black;}             
+     
+    th, td  {padding: 5px; text-align:left;}  
+
+    th {background-color:cornflowerblue ;}
 
     </style>
+
+    
+     
 </head>
 <body>
     <h1>EXERCICIO - 04 </h1>
@@ -30,13 +29,10 @@ $linguagems = [
     "JS"   => "Comportamentos",
     "PHP"  => "Back-end",
     "SQL"  => "Manipulação de dados",
-    "JAVA" => "Softwares"
-
-]; 
-
-?> 
-
-<table> 
+    "JAVA" => "Softwares" 
+];      
+?>  
+<table border="1"> 
 <tbody> 
 <tr> 
     <th>ID</th>
@@ -46,24 +42,80 @@ $linguagems = [
 <?php 
 $id = 1;
 foreach ($linguagems as $linguagem => $descricao) {
-?> 
-
-       
-        
+?>                   
         <tr> 
-            <td><?=$id?></td>
+            <td><?=$id++;?></td>
             <td><?=$linguagem?></td> 
             <td><?=$descricao?></td>
-        </tr>
-    
-
-<?php 
-$id++;
+        </tr>       
+<?php  
 }
 ?> 
 </tr>
 </tbody>
-</table>    
+</table> 
+
+<hr> 
+<h2>exemplo de outro array </h2> 
+<p>array mais estruturado parecido com as Estruturação do banco de dados.</p>
+<?php
+$linguagens = [
+    [
+        "id" => 1,
+        "nome" => "HTML",
+        "descricao" => "Estruturação"
+    ],
+    [
+        "id" => 2,
+        "nome" => "CSS",
+        "descricao" => "Estilos"
+    ],
+    [
+        "id" => 3,
+        "nome" => "JS",
+        "descricao" => "Comportamentos"
+    ],
+    [
+        "id" => 4,
+        "nome" => "PHP",
+        "descricao" => "Back-End"
+    ],
+    [
+        "id" => 5,
+        "nome" => "SQL",
+        "descricao" => "Manipulação de dados"
+    ],
+    [
+        "id" => 6,
+        "nome" => "Java",
+        "descricao" => "Softwares"
+    ],
+]; 
+?>
+
+<table border="1"> 
+<tbody> 
+<tr> 
+    <th>ID</th>
+    <th>linguagem</th>
+    <th>Descrição</th> 
+<?php 
+foreach ($linguagems as $linguagem ) {
+?>                  
+        <tr> 
+            <td><?=//$linguagem["id"]?></td>
+            <td><?=$linguagem["nome"]?></td> 
+            <td><?=$linguagem["descricao"]?></td>
+        </tr>       
+ <?php    
+} 
+?> 
+</tr>
+</tbody>
+</table> 
+
+
+
     
 </body>
 </html>
