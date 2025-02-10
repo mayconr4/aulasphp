9bb01c026aac43f1394f648b09807d0e9def54b8
mayconr4 <mayconhs2007@gmail.com>
2025-02-10 11:35:17 -0300

    adiciona bottstrap

--- 04-objetos-genericos.php
+++ 04-objetos-genericos.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>PHP - Objetos</title>
 </head>
 <body>
-    <h1>Trabalhando com objetos genéricos</h1>
+    <h1 class="text-center">Trabalhando com objetos genéricos</h1>
     <hr>
 
 <?php
@@ -53,7 +54,9 @@ $aluno = ["nome" => "Fulano", "sobrenome" => "dos Santos"];
 $objAluno = (object) $aluno;
 ?>
     <h3>Analisando o objeto gerado a partir de um array</h3>
-    <pre><?=var_dump($objAluno)?></pre>
+    <pre><?=var_dump($objAluno)?></pre> 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     
 </body>
