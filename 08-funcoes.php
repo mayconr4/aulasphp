59699e626c4ec959c70b3a004bc8ba406fee5588
mayconr4 <mayconhs2007@gmail.com>
2025-02-10 11:53:12 -0300

    exemplo de procedimento

--- 08-funcoes.php
+++ 08-funcoes.php
@@ -3,15 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <title>PHP - </title>
+    <title>PHP - Funções</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     
 </head>
 <body>  
     <div class="container">
-        <h1></h1>
-        <hr>
+        <h1>Trabalhando com funções</h1>
+        <hr> 
+
+
+        <h2>Função como procedimento (ou sub-rotina)</h2>
+        <p><i>Procedimentos <b>não retornam</b> nada.</i></p>
+<?php
+function exibirDaddosDoAutor(){
+    echo "<h4>Fulano de tal</h4>";
+    echo "<p><time>".date("d/m/Y")."</time></p>";
+}        
+?> 
+
+    <h3>Chamada de procedimento</h3>
+    <div> <?=exibirDaddosDoAutor();?> </div>
+
+    <hr>
     </div>
 
 
