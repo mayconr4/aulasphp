f71b2cefdc75c080c8de5ba662339a935b7b7b93
mayconr4 <mayconhs2007@gmail.com>
2025-02-13 11:31:19 -0300

    adiciona include

--- 10-include-e-require.php
+++ 10-include-e-require.php
@@ -1,9 +1,11 @@
+<?php include "recursos.php"; ?> 
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>PHP - Modularização e inclusão de recursos </title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     
@@ -11,7 +13,22 @@
 <body>  
     <div class="container">
         <h1>Modularização e inclusão de recursos</h1>
-        <hr>
+        <hr> 
+    <!-- Acessando a constante -->
+        <h2> <?=ESCOLA?> </h2> 
+        <!-- Acessando a variavel -->
+        <p> Estamos fazendo o curso de <?=$curso?> </p> 
+        <ul> 
+            <!-- Acessando o array -->
+            <?php foreach($tecnologias as $tecnologia) { ?>
+                <li><?=$tecnologia?></li>
+          <?php  }   ?>
+        </ul>  
+
+        <hr> 
+
+        <p> O fulano de tal tem 20 anos e é <?=verificarIdade(20)?>de idade</p>
+
     </div>
 
 
