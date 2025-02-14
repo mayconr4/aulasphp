03ec0494f072da833784d3f64972c4d43d1fe2fe
mayconr4 <mayconhs2007@gmail.com>
2025-02-14 08:33:30 -0300

    cria curso

---
+++ 11-site/cursos.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>cursos - site  com PHP - </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    
+</head>
+<body>  
+    <div class="container">
+       <header> 
+        <h1>Site com PHP</h1>  
+        <hr>
+        <nav>
+            <a href="index.php">Home</a> 
+            <a href="cursos.php">Cursos</a>
+            <a href="duvidas.php">Dúvidas</a> 
+        </nav>
+    </header> 
+
+       <main> 
+            <h2>Página de cursos </h2>
+            <p>Veja os cursos diponiveis:</p>
+            <ul class=" list-group">
+                <li class=" list-group-item">Culinária</li>
+                <li class=" list-group-item">Direção Defensiva</li>
+                <li class=" list-group-item">Como escapara de um terremoto</li>
+            </ul>
+       </main>
+       <footer> 
+            <p>Site criado para o curso Té. Informática para Internet</p>
+       </footer>
+    </div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+</body>
+</html>
