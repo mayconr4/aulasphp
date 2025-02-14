27b2453576898e80c097c22811ac3fcf339e3fa3
mayconr4 <mayconhs2007@gmail.com>
2025-02-14 11:07:11 -0300

    feito exercicio 06

--- 11-site/includes/cabecalho.php
+++ 11-site/includes/cabecalho.php
@@ -1,23 +1,55 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
+
+    <?php
+    $pagina = basename($_SERVER["PHP_SELF"]);
+    switch ($pagina) {
+        case 'index.php':
+            $titulo = "'Pagina inicial";
+            break;
+        case 'cursos.php':
+            $titulo = "Treinamentos";
+            break;
+        case 'duvidas.php':
+            $titulo = "Duvidas";
+            break;
+        default:
+            $titulo = "Contato";
+            break;
+    }
+    ?>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <title>Site com PHP - </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?= $titulo ?> -Site com PHP </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    
-</head>
-<body>  
-    <div class="container">
-       <header> 
-        <h1>Site com PHP</h1>  
-        <hr>
-        <nav>
-            <a href="index.php">Home</a> 
-            <a href="cursos.php">Treinamentos</a>
-            <a href="duvidas.php">Dúvidas</a> 
-        </nav>
-    </header> 
 
-       <main>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>Site com PHP</h1>
+
+            <nav>
+                <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="cursos.php">Treinamentos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="duvidas.php">duvidas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contato.php">contato</a>
+                    </li>
+
+                </ul>
+            </nav>
+            <hr>
+        </header>
+        <main>
--- 11-site/includes/rodape.php
+++ 11-site/includes/rodape.php
@@ -1,12 +1,18 @@
 </main>
-       <footer> 
-            <p>Site criado para o curso Té. Informática para Internet</p>
-       </footer>
-    </div>
+<?php
+date_default_timezone_set('America/sao_paulo');
+$dataHora = date('d/m/Y H:i');
+?>
+<footer>
+     <p>Site criado para o curso Té. Informática para Internet</p>
+     <p><time><?= $dataHora?></time></p>
+</footer>
+</div>
 
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    
+
 </body>
+
 </html>
