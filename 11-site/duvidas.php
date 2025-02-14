a402f280fb1eb54e8b92d3ee86a9ef532c54ebeb
mayconr4 <mayconhs2007@gmail.com>
2025-02-14 08:37:07 -0300

    cria pagina duvidas

---
+++ 11-site/duvidas.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Duvidas - site com PHP - </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>Site com PHP</h1>
+            <hr>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="cursos.php">Cursos</a>
+                <a href="duvidas.php">Dúvidas</a>
+            </nav>
+        </header>
+
+        <main>
+            <h2>Página de duvidas </h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste corporis, quasi, natus doloremque earum distinctio harum vitae velit similique, ducimus eligendi facere fuga aut eius voluptatibus rem maxime. Cumque, quisquam.</p>
+
+            <h3>Principais duvidas</h3>
+            <details>
+                <summary>como se cadastar</summary>
+                <p>Lorem numquam quae a deserunt atque delectus, deleniti maiores nostrum aliquid laboriosam vero consequuntur porro quidem animi.</p>
+            </details>
+
+            <details>
+                <summary>Os cursos são gratuitos</summary>
+                <p>Lorem ipsum dolorreiciendis numquam quae a deserunt atque delectus, deleniti maiores nostrum aliquid laboriosam vero consequuntur porro quidem animi.</p>
+            </details>
+
+        </main>
+        <footer>
+            <p>Site criado para o curso Té. Informática para Internet</p>
+        </footer>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
