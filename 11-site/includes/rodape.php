a7b557ae8cfb67d59b95076398ef5cca183de228
mayconr4 <mayconhs2007@gmail.com>
2025-02-14 11:31:19 -0300

    adiciona classe intl

--- 11-site/includes/rodape.php
+++ 11-site/includes/rodape.php
@@ -1,11 +1,24 @@
 </main>
 <?php
-date_default_timezone_set('America/sao_paulo');
-$dataHora = date('d/m/Y H:i');
+// date_default_timezone_set('America/sao_paulo');
+// $dataHora = date('d/m/Y H:i'); 
+
+
+// Definir o locale como 'pt_BR' (português do Brasil)
+setlocale(LC_TIME, 'pt_BR.UTF-8');
+
+// Criar um objeto DateTime
+$data = new DateTime();
+
+// Usar IntlDateFormatter para formatar a data por extenso
+$fmt = new IntlDateFormatter('pt_BR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+$data_extenso = $fmt->format($data);  
+
+ 
 ?>
-<footer>
+<footer class=" text-center">
      <p>Site criado para o curso Té. Informática para Internet</p>
-     <p><time><?= $dataHora?></time></p>
+     <p><time><?= $data_extenso?></time></p>
 </footer>
 </div>
 
