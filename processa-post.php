3be8314766d1fdb539d0e31d22be39faa30cd7a7
mayconr4 <mayconhs2007@gmail.com>
2025-02-18 09:11:12 -0300

    adiciona coalecencia nula

--- processa-post.php
+++ processa-post.php
@@ -1,39 +1,67 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Processamneto POST </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    
+
 </head>
-<body>  
+
+<body>
     <div class="container">
         <h1>Processamento usando POST</h1>
-        <hr> 
-        
-    </div> 
-<?php 
-// Capturando os dados transmistidos
-$nome = $_POST["nome"];
-$email = $_POST["email"];
-$idade = $_POST["idade"];
-$mensagem = $_POST["mensagem"]; 
-?> 
+        <hr>
 
-    <h2>Dados:</h2> 
+    </div>
+    <?php
+    // Capturando os dados transmistidos
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $idade = $_POST["idade"];
+    $mensagem = $_POST["mensagem"];
+
+    // capturando os options 
+
+    //solução 1: aplicar um ternario checando se existe algum interesse
+    //$interesses = isset($_POST["interesses"]) ? $_POST["interesses"] : []; 
+
+    // Solução 2 : usando operador de coalesc~encia nula ?? 
+    /* Se houver interesses, os armezene. Caso contrário, guarde um array vazio.*/ 
+    $interesses = $_POST["interesses"] ??  [];
+
+    ?>
+    <!-- Exibindo: -->
+    <h2>Dados:</h2>
 
     <ul>
-        <li>Nome:  <?=$nome?></li>
-        <li>E-mail <?=$email?></li>
-        <li>Idade: <?=$idade?></li>
-        <li>Mensagem: <?=$mensagem?></li>
+        <li>Nome: <?= $nome ?></li>
+        <li>E-mail <?= $email ?></li>
+        <li>Idade: <?= $idade ?> anos</li>
+
+        <li>Interesses (usando <code>implode()</code>)
+            <!-- TRanfrmando o array em string -->
+            <?=implode(", ", $interesses)?>
+        </li>
+
+        <li> Interesses- usando <code>foreach()</code>
+            <ul>
+                <?php foreach ($interesses as $interesse) { ?>
+                    <li><?=$interesse?></li> 
+                <?php  } ?>
+
+            </ul>
+        </li>
+
+        <li>Mensagem: <?= $mensagem ?></li>
     </ul>
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </body>
+
 </html>
